e290ab1cbaae23411440e1979a12e42ebc872105
mwaniki Tifany <111235348+RivoltaAlpha@users.noreply.github.com>
2024-01-25 23:43:03 +0000

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Tifany Nyawira",
+  title: "Web Designer & Python Developer",
+  email: "mwanikitiffany@gmail.com",
+  gitHub: "RivoltaAlpha",
+  instagram: "",
+  linkedIn: "Mwaniki Tifany",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "RivoltaAlpha",
+  youTube: "Coder",
 };
 
 const primaryColor = "#4E567E";
